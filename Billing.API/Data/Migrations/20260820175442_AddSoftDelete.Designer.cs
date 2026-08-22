7514d395cd270d3949a8ce9697252823a238f3bd
Henrique Vasques <henriquevasques04@gmail.com>
2026-08-22 09:42:34 -0300

    Migration para criação da propiedade IsDeleted nas entidades, para implementar o Soft Delete.

--- Billing.API/Data/Migrations/20260820175442_AddSoftDelete.Designer.cs
+++ Billing.API/Data/Migrations/20260820175442_AddSoftDelete.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Billing.API.Data.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Billing.API.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260820175442_AddSoftDelete")]
+    partial class AddSoftDelete
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
