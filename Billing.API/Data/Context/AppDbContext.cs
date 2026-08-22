bc62410187e028dc54d04ef65faf6aba59605af4
Henrique Vasques <henriquevasques04@gmail.com>
2026-08-22 09:39:40 -0300

    Tornando Unique a propiedade Number de Invoice.

--- Billing.API/Data/Context/AppDbContext.cs
+++ Billing.API/Data/Context/AppDbContext.cs
@@ -8,5 +8,13 @@ namespace Billing.API.Data.Context
         public AppDbContext(DbContextOptions<AppDbContext> dbContextOptions) : base(dbContextOptions) { }
         public DbSet<Invoice> Invoices { get; set; }
         public DbSet<InvoiceItem> InvoiceItems { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Invoice>()
+                .HasIndex(i => i.Number)
+                .IsUnique();
+        }
     }
 }
